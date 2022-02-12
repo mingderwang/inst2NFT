aa2632f781d7899ecad0a0d038336ee5d5963a88
Ming-der Wnag <mingderwang@gmail.com>
2022-02-12 14:14:01 +0800

    add Terms of Service

---
+++ packages/frontend/pages/terms-of-service.js
@@ -0,0 +1,40 @@
+export default function Service() {
+  return (
+    <section class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+      <div class="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
+        <p class="text-xs font-bold text-left text-pink-500 uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold">
+          To use on your own risk and responsibility.
+        </p>
+        <h3 class="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
+          Terms of Service
+        </h3>
+        <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+          <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">
+            To know what you are doing!
+          </h3>
+          <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+            NFT is a very new term in the digital world. And most of the people
+            don't really know what the NFT really is. We just provide the tool
+            for you to create a NFT (token) with a metadata to link to your
+            (own) media or any other digital contents. That's it. When you own
+            the token, you are the owner of the token. It doesn't really mean
+            you are the owner or creater of the original contents. Everyone
+            should be ware of the copyright issues and the NFT legally means.
+            This tool or service is just help you to create a NFT from your
+            posted media on Instagram or Facebook.
+          </p>
+        </div>
+        <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+          <h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">
+            What you can do with these NFTs?
+          </h3>
+          <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+            You can own and keep your ownership of NFTs, or trading them on NFT
+            marketplaces like OpenSea. Or even simplly transfer them to any
+            other person via any Ethereum compatible wallet, such as Metamask.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
