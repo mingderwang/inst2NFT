f35df86c35dc4e809d0f80be53d3baa0e2d70829
Ming-der Wnag <mingderwang@gmail.com>
2022-02-12 14:20:14 +0800

    minor change for TOS

--- packages/frontend/pages/terms-of-service.js
+++ packages/frontend/pages/terms-of-service.js
@@ -14,14 +14,14 @@ export default function Service() {
           </h3>
           <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
             NFT is a very new term in the digital world. And most of the people
-            don't really know what the NFT really is. We just provide the tool
-            for you to create a NFT (token) with a metadata to link to your
-            (own) media or any other digital contents. That's it. When you own
-            the token, you are the owner of the token. It doesn't really mean
-            you are the owner or creater of the original contents. Everyone
-            should be ware of the copyright issues and the NFT legally means.
-            This tool or service is just help you to create a NFT from your
-            posted media on Instagram or Facebook.
+            don't really know what the NFT really is. We just provide a tool for
+            you to create a NFT (token) with a metadata to link to your (own)
+            media or any other digital contents. That's it. When you own the
+            token, you are the owner of the token. It doesn't really mean you
+            are the owner or creater of the original contents. Everyone should
+            be ware of the copyright issues and the NFT legally means. This tool
+            or service is just help you to create a NFT from your posted media
+            on Instagram or Facebook.
           </p>
         </div>
         <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
@@ -29,9 +29,10 @@ export default function Service() {
             What you can do with these NFTs?
           </h3>
           <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
-            You can own and keep your ownership of NFTs, or trading them on NFT
-            marketplaces like OpenSea. Or even simplly transfer them to any
-            other person via any Ethereum compatible wallet, such as Metamask.
+            After you convert your media to NFTs, you can own and keep your
+            ownership of NFTs, or trading them on NFT marketplaces like OpenSea.
+            Or even simplly transfer them to any other person via any Ethereum
+            compatible wallet, such as Metamask.
           </p>
         </div>
       </div>
