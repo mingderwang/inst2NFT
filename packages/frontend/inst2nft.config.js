32a59c2fc3f9140d86812d02d2a1a56cdd5c4cc7
Ming-der Wang <mingderwang@gmail.com>
2022-03-30 10:24:08 +0800

    v2.0.0-frontend-rinkey-rc2

--- packages/frontend/inst2nft.config.js
+++ packages/frontend/inst2nft.config.js
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const defaultNetwork = "polygon"; // or "polygon"
+const defaultNetwork = "rinkeby"; // or "polygon"
 const TestnetOpenSeaURL = "https://testnets.opensea.io/collection/inst2nft";
 const MainnetOpenSeaURL = "https://opensea.io/collection/inst2nft";
 
@@ -9,7 +9,7 @@ const config = {
   defaultNetwork,
   networks: {
     rinkeby: {
-      contractAddress: "0x074DB1360727588E9D3cFd1fef79a9573037B68e",
+      contractAddress: "0x8b756D45dD3a77124a0E762C7023e094cc035B40",
       openseaURL: TestnetOpenSeaURL,
     },
     polygon: {
