d6b95d41eac7c2f195ae496a21ced432389ded22
Ming-der Wang <mingderwang@gmail.com>
2022-03-30 19:48:09 +0800

    move GDPR to top of the window

--- packages/frontend/pages/_app.js
+++ packages/frontend/pages/_app.js
@@ -39,35 +39,35 @@ const MyApp = ({ Component, pageProps }) => {
         >
           <Layout>
             <ConnectWallet />
+            {gdpr && (
+              <div className="alert shadow-lg">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="stroke-info flex-shrink-0 w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                  <span>
+                    This website uses cookies to improve your user experience.
+                  </span>
+                </div>
+                <div className="flex-none">
+                  <button className="btn btn-sm btn-primary" onClick={onAccept}>
+                    Got It
+                  </button>
+                </div>
+              </div>
+            )}
           </Layout>
           <Component {...pageProps} />
-          {gdpr && (
-            <div className="alert shadow-lg">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="stroke-info flex-shrink-0 w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
-                <span>
-                  This website uses cookies to improve your user experience.
-                </span>
-              </div>
-              <div className="flex-none">
-                <button className="btn btn-sm btn-primary" onClick={onAccept}>
-                  Got It
-                </button>
-              </div>
-            </div>
-          )}
         </SessionProvider>
       </RecoilRoot>
     </React.StrictMode>
