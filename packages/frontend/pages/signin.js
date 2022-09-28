2f9698f4a0ebdc811d7a43c0c7da5cfa2d84410f
Ming-der Wang <mingderwang@operamail.com>
2022-09-28 23:53:51 +0800

    v2.2.2 fix horizonal signin page to vertical

--- packages/frontend/pages/signin.js
+++ packages/frontend/pages/signin.js
@@ -3,27 +3,30 @@ import { getProviders, getSession, signIn } from "next-auth/react";
 export default function SignIn({ providers }) {
   return (
     <>
-      <section className="mt-8 container mx-auto flex items-center justify-around">
-        {Object.values(providers).map((provider) => (
-          <button
-            className={
-              provider.name === "Instagram"
-                ? "btn btn-secondary btn-lg"
-                : "btn btn-primary btn-lg"
-            }
-            onClick={() => signIn(provider.id)}
-          >
-            <img
-              className="inline-block w-12 h-12 mr-2 stroke-current"
-              src={
+      <section className="container mx-auto flex items-center justify-around">
+        <div class="grid grid-cols-1 gap-4">
+          {Object.values(providers).map((provider) => (
+            <button
+              key={provider.id}
+              className={
                 provider.name === "Instagram"
-                  ? "/Instagram_Glyph_Gradient_RGB.svg"
-                  : "/FB-RGB-Wht.svg"
+                  ? "btn btn-secondary btn-lg"
+                  : "btn btn-primary btn-lg"
               }
-            ></img>
-            Continue with {provider.name}
-          </button>
-        ))}
+              onClick={() => signIn(provider.id)}
+            >
+              <img
+                className="inline-block w-12 h-12 mr-2 stroke-current"
+                src={
+                  provider.name === "Instagram"
+                    ? "/Instagram_Glyph_Gradient_RGB.svg"
+                    : "/FB-RGB-Wht.svg"
+                }
+              ></img>
+              Continue with {provider.name}
+            </button>
+          ))}
+        </div>
       </section>
     </>
   );
